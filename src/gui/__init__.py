9df2a0a469a6e204e74c1d06262db3b390a635c6
Vladislav Ogai <ogaivl@my.msu.ru>
2026-07-30 00:34:07 +0300

    Update gui init file with new objects

---
+++ src/gui/__init__.py
@@ -0,0 +1,18 @@
+from .theme import (
+    init_fonts,
+    build_stylesheet,
+    HEADING_POINT_SIZE,
+    BODY_POINT_SIZE,
+    COLOR_BG,
+    COLOR_SCREEN_BG,
+    COLOR_PRIMARY,
+    COLOR_TEXT,
+    COLOR_SECONDARY,
+    COLOR_ACCENT,
+    COLOR_ERROR,
+    COLOR_NUMBER,
+    COLOR_BTN_BG,
+)
+from .terminal_view import TerminalView
+from .wizard_view import WizardView
+from .state_manager import StateManager
